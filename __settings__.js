09f55df56cb234ad0b3e54588b22dfac9b4b7754
Nawsomey <111446394+Nawsomey@users.noreply.github.com>
2024-04-11 23:25:27 -0400

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1982218.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 174327073, 174327801, 174327510 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/174328258/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/174328259/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/174328257/1/ammo.js', 'preload' : true},
+];
